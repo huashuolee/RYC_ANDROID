b2ba3943a77b5aa50d126fa7b152bf6e9dc6f891
huashuolee <huashuolee@gmail.com>
2015-07-10 16:46:26 +0800

    Learn fragment

--- LearnFragment/app/src/main/java/com/goafter/learnfragment/MainActivity.java
+++ LearnFragment/app/src/main/java/com/goafter/learnfragment/MainActivity.java
@@ -1,28 +1,74 @@
 package com.goafter.learnfragment;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private Button addFragment,rmFrament,rpFragment;
+    private MainActivityFragment fragment;
+    private sdFragment sdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.fragment_container, new MainActivityFragment())
-                    .commit();
-        }
+        fragment = new MainActivityFragment();
+        sdFragment = new sdFragment();
+        findViewById(R.id.addFragment).setOnClickListener(this);
+        findViewById(R.id.rmFragment).setOnClickListener(this);
+        findViewById(R.id.rpFragment).setOnClickListener(this);
+        findViewById(R.id.btnSlider).setOnClickListener(this);
 
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.addFragment:
+                addFragments();
+                break;
+            case R.id.rmFragment:
+                rmFragments();
+                break;
+            case R.id.rpFragment:
+                rpFragments();
+                break;
+            case R.id.btnSlider:
+                startActivity(new Intent(this, SliderActivity.class));
+        }
+
+    }
+
+    public void addFragments(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+
+    }
+    public void rmFragments(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .remove(fragment)
+                .addToBackStack(null)
+                .commit();
+
+    }
+    public void rpFragments(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, sdFragment)
+                .commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ LearnFragment/app/src/main/java/com/goafter/learnfragment/sdFragment.java
@@ -0,0 +1,36 @@
+package com.goafter.learnfragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+
+
+public class sdFragment extends Fragment {
+
+    public sdFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sd, container, false);
+    }
+
+
+
+
+}
