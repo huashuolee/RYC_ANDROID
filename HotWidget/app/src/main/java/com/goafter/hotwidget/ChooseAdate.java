11e9587c98f8602a2e9278b976dcf2c437731431
huashuolee <huashuolee@gmail.com>
2015-07-21 17:40:49 +0800

    DatePickerDialog

---
+++ HotWidget/app/src/main/java/com/goafter/hotwidget/ChooseAdate.java
@@ -0,0 +1,58 @@
+package com.goafter.hotwidget;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+
+public class ChooseAdate extends ActionBarActivity {
+    private Button btnChooseDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_adate);
+        btnChooseDate = (Button)findViewById(R.id.btnChooseDate);
+        btnChooseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ChooseAdate.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String date = String.format("%d-%d-%d",year,monthOfYear+1,dayOfMonth);
+                        btnChooseDate.setText(date);
+
+                    }
+                },2015,3,15).show();
+                
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_choose_adate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- HotWidget/app/src/main/java/com/goafter/hotwidget/MainActivity.java
+++ HotWidget/app/src/main/java/com/goafter/hotwidget/MainActivity.java
@@ -1,14 +1,18 @@
 package com.goafter.hotwidget;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,10 +27,28 @@ public class MainActivity extends ActionBarActivity {
                 this, R.array.planets, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
 
-        String[] mItems = getResources().getStringArray(R.array.planets);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,mItems);
+/*        String[] mItems = getResources().getStringArray(R.array.planets);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,mItems);*/
+        int resource = android.R.layout.simple_dropdown_item_1line;
+        final String[] mItems = getResources().getStringArray(R.array.planets);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,resource,mItems);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),mItems[position],Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
