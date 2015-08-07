b0d785cbbb94fb5c7e8bdc578e83f0a5088f7ebf
huashuolee <huashuolee@gmail.com>
2015-08-07 17:29:49 +0800

    dailyupdate DrawerLayout

--- LearningDrawerLayout/app/src/main/java/com/goafter/learningdrawerlayout/MainActivity.java
+++ LearningDrawerLayout/app/src/main/java/com/goafter/learningdrawerlayout/MainActivity.java
@@ -1,14 +1,21 @@
 package com.goafter.learningdrawerlayout;
 
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SearchViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
@@ -19,13 +26,16 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ActionBarDrawerToggle mDrawerToggle;
     private String mTitle;
-
+    private ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTitle = (String)getTitle();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.draw_layout);
+        mTitle = (String)getTitle();
+        actionBar = getSupportActionBar();
+
+        Log.e("A", mTitle);
        // listView = new ListView(this);
         listView = (ListView) findViewById(R.id.left_drawer);
         arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,getData());
@@ -34,16 +44,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getActionBar().setTitle("");
+                actionBar.setTitle("");
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-                getActionBar().setTitle(mTitle);
+                actionBar.setTitle(mTitle);
             }
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        //开启actionbar APP icon 功能
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
 
     }
 
@@ -68,13 +83,23 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
+        switch (item.getItemId()){
+            case R.id.action_search:
+                Intent intent = new Intent(Intent.ACTION_SEARCH);
+                startActivity(intent);
+        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mDrawerToggle.syncState();
     }
 }
