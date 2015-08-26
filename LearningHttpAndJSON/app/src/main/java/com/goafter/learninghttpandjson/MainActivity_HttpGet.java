8b55029216d6080bef81bb7e08e98c95d4f1d39b
huashuolee <huashuolee@gmail.com>
2015-08-26 17:45:30 +0800

    daily update http json

---
+++ LearningHttpAndJSON/app/src/main/java/com/goafter/learninghttpandjson/MainActivity_HttpGet.java
@@ -0,0 +1,110 @@
+package com.goafter.learninghttpandjson;
+
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity_HttpGet extends AppCompatActivity {
+
+    Button btnSearch;
+    TextView tvResult;
+    EditText etWord;
+    String line,url;
+    StringBuilder builder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity__http_get);
+        btnSearch  = (Button) findViewById(R.id.btnSearch);
+        tvResult = (TextView)findViewById(R.id.tvDisplayResult);
+        etWord = (EditText)findViewById(R.id.etWord);
+        btnSearch.setOnClickListener(new Search());
+
+    }
+
+    class Search implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            String word = etWord.getText().toString();
+            url = UrlConst.YOUDAOAPI + word;
+            update();
+
+        }
+
+        public void update(){
+
+            new AsyncTask<String,Void,StringBuilder>(){
+                @Override
+                protected StringBuilder doInBackground(String... params) {
+                    try {
+                        URL url = new URL(params[0]);
+                        URLConnection connection = url.openConnection();
+                        InputStream is = connection.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is,"utf-8");
+                        BufferedReader br = new BufferedReader(isr);
+                        builder = new StringBuilder();
+
+                        while((line = br.readLine()) != null){
+                            builder.append(line);
+                            Log.e("1111111",line);
+                        }
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return builder;
+                }
+
+                @Override
+                protected void onPostExecute(StringBuilder stringBuilder) {
+                    super.onPostExecute(stringBuilder);
+                    try {
+                        JSONObject jsonObject = new JSONObject(builder.toString()).getJSONObject("basic");
+
+                        String[] explains = new String[]{jsonObject.getString("explains")};
+                        List<String> list = new ArrayList<String>();
+
+
+                        tvResult.setText(explains[0]);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                }
+            }.execute(url);
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ LearningHttpAndJSON/app/src/main/java/com/goafter/learninghttpandjson/UrlConst.java
@@ -0,0 +1,16 @@
+package com.goafter.learninghttpandjson;
+
+/**
+ * Created by huashuolee on 2015/8/26.
+ */
+public interface UrlConst {
+
+    //有道词典
+    public static final String YOUDAOAPI = "http://fanyi.youdao.com/openapi.do?keyfrom=goafter&key=194169332&type=data&doctype=json&version=1.1&q=";
+    public static final String YOUDAOPRON ="http://dict.youdao.com/dictvoice?audio=deprecated";
+
+    //Taobao 号码归属地
+    //天气预报
+    //快递
+    //
+}
