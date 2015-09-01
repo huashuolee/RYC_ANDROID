2aa21d7426f2a33ea1c379b66a8ce3ce61167358
huashuolee <huashuolee@gmail.com>
2015-09-01 20:02:14 +0800

    daily update transformers

--- TransformersToolkit/app/src/main/java/com/goafter/transformerstoolkit/MainActivity.java
+++ TransformersToolkit/app/src/main/java/com/goafter/transformerstoolkit/MainActivity.java
@@ -1,16 +1,38 @@
 package com.goafter.transformerstoolkit;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mPlanetTitles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        ArrayAdapter <String > lfadapter = new ArrayAdapter<String>(this,R.layout.drawer_list_item,mPlanetTitles);
+        mDrawerList.setAdapter(lfadapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
     }
 
     @Override
