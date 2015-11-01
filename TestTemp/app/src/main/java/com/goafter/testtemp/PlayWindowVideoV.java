5e7bb54c91bd6db3c6c009f3460162b0d7f0b6c8
huashuolee <huashuolee@gmail.com>
2015-11-01 22:26:25 +0800

    update video play

--- TestTemp/app/src/main/java/com/goafter/testtemp/PlayWindowVideoV.java
+++ TestTemp/app/src/main/java/com/goafter/testtemp/PlayWindowVideoV.java
@@ -1,16 +1,14 @@
 package com.goafter.testtemp;
 
-import android.content.Context;
+
 import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.MediaController;
-import android.widget.VideoView;
 
 public class PlayWindowVideoV extends AppCompatActivity {
 
@@ -37,36 +35,6 @@ public class PlayWindowVideoV extends AppCompatActivity {
     }
 
 
-    public class CustomVideoView extends VideoView {
-        private int mVideoWidth;
-        private int mVideoHeight;
 
-        public CustomVideoView(Context context) {
-            super(context);
-            // TODO Auto-generated constructor stub
-        }
-
-        public CustomVideoView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            // TODO Auto-generated constructor stub
-        }
-
-        public CustomVideoView(Context context, AttributeSet attrs, int defStyle) {
-            super(context, attrs, defStyle);
-            // TODO Auto-generated constructor stub
-        }
-
-        @Override
-        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            // TODO Auto-generated method stub
-            // Log.i("@@@@", "onMeasure");
-
-            //下面的代码是让视频的播放的长宽是根据你设置的参数来决定
-
-            int width = getDefaultSize(mVideoWidth, widthMeasureSpec);
-            int height = getDefaultSize(mVideoHeight, heightMeasureSpec);
-            setMeasuredDimension(width, height);
-        }
-    }
 
 }
--- TestTemp/app/src/main/java/com/goafter/testtemp/TTPlayer.java
+++ TestTemp/app/src/main/java/com/goafter/testtemp/TTPlayer.java
@@ -28,57 +28,26 @@ public class TTPlayer extends AppCompatActivity {
     class myclicklistener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            System.err.println("555555555555");
 
             switch (v.getId()) {
                 case R.id.btnPlay:
-                    System.err.println("11111111");
                     Intent intent = new Intent(TTPlayer.this, PlayWindow.class);
                     Bundle bundle = new Bundle();
-                    bundle.putString("uri","file:///sdcard/video1.mp4");
+                    bundle.putString("uri", "file:///sdcard/video1.mp4");
                     intent.putExtras(bundle);
                     startActivity(intent);
-                    System.err.println("2222222222");
                     break;
                 case R.id.btnPause:
-                    System.err.println("3333333333333333");
                     Intent intentv = new Intent(TTPlayer.this, PlayWindowVideoV.class);
                     Bundle bundlev = new Bundle();
-                    bundlev.putString("uri","file:///sdcard/video1.mp4");
+                    bundlev.putString("uri", "file:///sdcard/video1.mp4");
                     intentv.putExtras(bundlev);
                     startActivity(intentv);
-                    System.err.println("44444444444444444444");
                     break;
                 default:
                     break;
-
-
             }
-
-
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ttplayer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
