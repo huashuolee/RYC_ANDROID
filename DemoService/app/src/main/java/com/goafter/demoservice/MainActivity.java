be136a88ddc7daed8e81e922cf7ec6a46d041907
huashuolee <huashuolee@gmail.com>
2015-11-26 17:44:24 +0800

    service

--- DemoService/app/src/main/java/com/goafter/demoservice/MainActivity.java
+++ DemoService/app/src/main/java/com/goafter/demoservice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.goafter.demoservice;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +11,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnStartService,btnStopService;
+    Button btnStartService,btnStopService,btnBind,btnUnBind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnStartService = (Button) findViewById(R.id.btnStartService);
         btnStopService = (Button) findViewById(R.id.btnStopService);
+        btnBind = (Button) findViewById(R.id.btnBindService);
+        btnUnBind = (Button) findViewById(R.id.btnUnBindService);
         btnStartService.setOnClickListener(this);
         btnStopService.setOnClickListener(this);
     }
@@ -29,6 +34,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnStopService:
                 Intent intent1 = new Intent(this, MyService.class);
                 stopService(intent1);
+                break;
+            case R.id.btnBindService:
+                Intent intentBinder = new Intent(this, MyService.class);
+                bindService(intentBinder, new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName name, IBinder service) {
+                        service.pingBinder();
+
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName name) {
+
+                    }
+                }, BIND_AUTO_CREATE);
+
+                break;
+            case R.id.btnUnBindService:
+
                 break;
             default:
                 break;
