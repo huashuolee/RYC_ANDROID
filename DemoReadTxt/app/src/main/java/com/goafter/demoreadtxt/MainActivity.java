1ea3ad6602099b79febe912d752cee4d6542f897
huashuolee <huashuolee@gmail.com>
2015-12-09 17:41:23 +0800

    update

--- DemoReadTxt/app/src/main/java/com/goafter/demoreadtxt/MainActivity.java
+++ DemoReadTxt/app/src/main/java/com/goafter/demoreadtxt/MainActivity.java
@@ -1,50 +1,62 @@
 package com.goafter.demoreadtxt;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends AppCompatActivity {
     StringBuilder sb = new StringBuilder();
     MyView myView;
+    String TAG = "=============";
+    BufferedReader br;
+    TextView tvDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myView = (MyView) findViewById(R.id.myView);
+        tvDisplay = (TextView) findViewById(R.id.tvDisplay);
+        //myView = (MyView) findViewById(R.id.myView);
         loadBook();
         loadPage();
-
-
     }
 
     private void loadPage() {
+        char[] buf = new char[3];
+        try {
+            br.read(buf);
+            String result = new String(buf);
+            Log.e(TAG, result);
+            tvDisplay.setTextSize(15);
+            tvDisplay.setText(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     private void loadBook() {
         String path = Environment.getExternalStorageDirectory().getPath() + "/a.txt";
         File file = new File(path);
-        String line;
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
+            br = new BufferedReader(new FileReader(file));
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"gbk"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
