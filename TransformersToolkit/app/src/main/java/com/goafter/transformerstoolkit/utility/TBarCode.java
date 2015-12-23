53b0c6e44bd7cc9e3deed60babf9d4043588c4d1
huashuolee <huashuolee@gmail.com>
2015-12-23 12:40:37 +0800

    integrate zxing to my appp

--- TransformersToolkit/app/src/main/java/com/goafter/transformerstoolkit/utility/TBarCode.java
+++ TransformersToolkit/app/src/main/java/com/goafter/transformerstoolkit/utility/TBarCode.java
@@ -1,9 +1,9 @@
 package com.goafter.transformerstoolkit.utility;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,9 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.goafter.transformerstoolkit.R;
+import com.goafter.transformerstoolkit.zxing.activity.CaptureActivity;
+import com.goafter.transformerstoolkit.zxing.encoding.EncodingHandler;
 import com.google.zxing.WriterException;
 
 
@@ -32,7 +35,8 @@ public class TBarCode extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-   }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,24 +49,66 @@ public class TBarCode extends Fragment {
         qrStrEditText = (EditText) view.findViewById(R.id.et_qr_string);
         qrImgImageView = (ImageView) view.findViewById(R.id.iv_qr_image);
 
-        Button scanBarCodeButton = (Button) view.findViewById(R.id.btn_scan_barcode);
-        scanBarCodeButton.setOnClickListener(new View.OnClickListener() {
+
+        resultTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str = "market://details?id=" + "com.google.zxing.client.android";
-                Intent localIntent = new Intent("android.intent.action.VIEW");
-                localIntent.setData(Uri.parse(str));
-                startActivity(localIntent);
 
+            }
+        });
+
+        Button scanBarCodeButton = (Button) view.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //打开扫描界面扫描条形码或二维码
+                Intent openCameraIntent = new Intent(getActivity(), CaptureActivity.class);
+                startActivityForResult(openCameraIntent, 0);
             }
         });
 
 
         Button generateQRCodeButton = (Button) view.findViewById(R.id.btn_add_qrcode);
+        generateQRCodeButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                try {
+                    String contentString = qrStrEditText.getText().toString();
+                    if (!contentString.equals("")) {
+                        //根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
+                        Bitmap qrCodeBitmap = EncodingHandler.createQRCode(contentString, 350);
+                        qrImgImageView.setImageBitmap(qrCodeBitmap);
+                    } else {
+                        Toast.makeText(getActivity(), "Text can not be empty", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (WriterException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
         return view;
     }
 
+
+
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //处理扫描结果（在界面上显示）
+        if (resultCode == getActivity().RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            resultTextView.setText(scanResult);
+        }
+    }
 
 
 
